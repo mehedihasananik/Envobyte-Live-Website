17d36430165f282c52c3653090d2d9e948dcc2b2
mehedihasananikoffcial <marshallanique@gmail.com>
2024-03-26 14:59:14 +0600

    question icon change

--- src/app/(home)/layout.js
+++ src/app/(home)/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "../globals.css";
 import { Footer } from "@/Components";
 import Header from "@/Components/Home/Navbar/Header";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/src/app/favicon.ico" sizes="any" />
+      </Head>
       <body className={inter.className}>
         <Header />
         {children}
